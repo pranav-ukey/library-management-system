c02cd888c7677088a69f8cbd401d106acf083970
Pranav Ukey <pranavukey641@gmail.com>
2026-06-27 10:11:00 +0530

    Complete book management CRUD APIs

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -36,6 +36,120 @@ const addBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+
+    res.status(200).json({
+      success: true,
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+const getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: book,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { title, author, isbn, category, quantity } = req.body;
+
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    book.title = title ?? book.title;
+    book.author = author ?? book.author;
+    book.isbn = isbn ?? book.isbn;
+    book.category = category ?? book.category;
+
+    // If quantity changes, adjust availableQuantity
+    if (quantity !== undefined) {
+      const borrowedBooks = book.quantity - book.availableQuantity;
+
+      book.quantity = quantity;
+      book.availableQuantity = quantity - borrowedBooks;
+    }
+
+    await book.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: book,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    await book.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
 module.exports = {
   addBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
 };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,11 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-const { addBook } = require("../controllers/bookController");
+const { addBook, getAllBooks, getBookById, updateBook, deleteBook,} = require("../controllers/bookController");
 
 const protect = require("../middleware/authMiddleware");
 const authorize = require("../middleware/roleMiddleware");
 
 router.post("/", protect, authorize("librarian"), addBook);
+router.get("/", protect, getAllBooks);
+router.get("/:id", protect, getBookById);
+router.put(
+  "/:id",
+  protect,
+  authorize("librarian"),
+  updateBook
+);
+
+router.delete(
+  "/:id",
+  protect,
+  authorize("librarian"),
+  deleteBook
+);
 
 module.exports = router;
